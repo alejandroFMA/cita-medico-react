8115e65e64552b7777dd3ceb1c163efe5d3ba57d
alejandroFMA <alexfmarquez@gmail.com>
2024-02-19 19:40:53 +0100

    change footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import github from "../../assets/github.png";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="flex justify-end text-md sticky bottom-0">
+      <div className="flex row-auto bg-gray-900 rounded-md items-center p-1">
+        <p className="text-white">
+          Realizado por{" "}
+          <a
+            href="https://github.com/alejandroFMA"
+            className="font-bold text-indigo-600 hover:text-indigo-800"
+            target="_blank"
+          >
+            Alejandro Márquez
+          </a>
+        </p>
+        <div className="bg-gray-900 rounded-md">
+        <a href="https://github.com/alejandroFMA" target="_blank">
+          <img
+            src={github}
+            alt="Alejandro Márquez's GitHub page"
+            width="24"
+            height="24"
+            className="ml-2 bg-white rounded-full"
+          />  
+        </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
